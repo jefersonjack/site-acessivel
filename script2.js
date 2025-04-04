87d0ae7fbf13f299b11a0a902d4975adaee71f91
Jerso <141340423+jefersonjack@users.noreply.github.com>
2025-04-04 07:55:47 -0300

    dhfdh

--- script2.js
+++ script2.js
@@ -16,7 +16,7 @@ const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-    )
+    })
 
     diminuiFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte -= 0.1;
